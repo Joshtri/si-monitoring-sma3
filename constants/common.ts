c8cbba3025378a6c326052053871e4439ed63ad5
Joshtri <stuffofyos1516@gmail.com>
2025-08-04 14:28:20 +0700

    Commit: common.ts

---
+++ constants/common.ts
@@ -0,0 +1,8 @@
+// src/constants/roles.ts
+
+export const Roles = [
+  { label: "Admin", value: "ADMIN" },
+  { label: "Guru", value: "GURU" },
+  { label: "Wali Kelas", value: "WALI_KELAS" },
+  { label: "Orang Tua", value: "ORANG_TUA" },
+];
