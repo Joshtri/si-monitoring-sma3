aa5ecaeaff264811e13aa41e740b55764ce4774d
Joshtri <stuffofyos1516@gmail.com>
2025-08-04 14:04:28 +0700

    add: create RootLayout component with global styles and metadata

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+import "@/styles/globals.css";
+import { Metadata } from "next";
+import clsx from "clsx";
+
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+// import "react-datepicker/dist/react-datepicker.css";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="id" className="light">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </head>
+      <body
+        className={clsx(
+          "min-h-screen text-foreground font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
+          <div className="relative flex flex-col min-h-screen">
+            <main className="flex-grow">{children}</main>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
