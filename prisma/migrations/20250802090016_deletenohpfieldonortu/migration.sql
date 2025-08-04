350ed2aadd6ddba107e9ca940e205dd29466b222
Joshtri <stuffofyos1516@gmail.com>
2025-08-04 14:28:21 +0700

    Commit: migration.sql

---
+++ prisma/migrations/20250802090016_deletenohpfieldonortu/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `noHp` on the `OrangTua` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."OrangTua" DROP COLUMN "noHp";
