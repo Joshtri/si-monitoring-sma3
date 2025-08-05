fb555f0c6e650b80e48c5f8b7d641c72813c613c
Joshtri <stuffofyos1516@gmail.com>
2025-08-05 23:37:41 +0700

    Commit: getAuthUser.ts

--- lib/auth/getAuthUser.ts
+++ lib/auth/getAuthUser.ts
@@ -6,6 +6,20 @@ export function getAuthUserFromCookie() {
     return token ? verifyToken(token) : null
 }
 
+
+// export async function getAuthUserFromCookie() {
+//   const cookieStore = await cookies(); // await the promise to get the cookie store
+//   const token = cookieStore.get("token")?.value;
+//   return token ? verifyToken(token) : null;
+// }
+
+// export async function getAuthUserFromCookie() {
+//   const cookieStore = cookies();
+//   const token = cookieStore.get("token")?.value;
+//   return token ? verifyToken(token) : null;
+// }
+
+
 export function getAuthUserFromHeader(req: Request) {
     const authHeader = req.headers.get("authorization")
     const token = authHeader?.split(" ")[1]
