f1f938090482710ae10cbc7044d88ea6cc914539
Joshtri <stuffofyos1516@gmail.com>
2025-08-05 23:37:41 +0700

    Commit: guruService.ts

--- services/guruService.ts
+++ services/guruService.ts
@@ -4,6 +4,25 @@ import { editGuruSchema } from "@/validations/guruSchema";
 import z from "zod";
 
 
+export interface AbsenSiswaItem {
+  siswaKelasId: string;
+  // namaDepan: string;
+  // namaTengah?: string;
+  // namaBelakang?: string;
+
+  nama: string;
+  nis: string;
+  status?: "HADIR" | "SAKIT" | "IZIN" | "ALPHA";
+}
+
+export interface KelasAbsensiHariIni {
+  kelasId: string;
+  kelas: string;
+  mapel: string;
+  jamKe: number;
+  siswa: AbsenSiswaItem[];
+}
+
 
 export type EditGuruValues = z.infer<typeof editGuruSchema>;
 
@@ -30,5 +49,59 @@ export async function updateGuruWithUser(data: EditGuruValues) {
 
 export async function deleteGuru(id: string) {
   const res = await api.delete(`/api/guru/${id}`);
+  return res.data;
+}
+
+
+
+/// Function to get Mata Pelajaran for Guru that is logged in
+
+export async function getGuruMataPelajaran() {
+  const res = await api.get("/api/guru/me/mata-pelajaran");
+  return res.data;
+}
+
+
+
+// Ambil daftar siswa untuk absensi hari ini
+export async function getSiswaHariIni(): Promise<KelasAbsensiHariIni[]> {
+  const res = await api.get("/api/guru/absensi/siswa-hari-ini");
+  return res.data;
+}
+
+// Ambil daftar kelas yang diajar hari ini
+export async function getKelasHariIni() {
+  const res = await api.get("/api/guru/absensi/kelas-hari-ini");
+  return res.data;
+}
+
+// Ambil daftar siswa berdasarkan kelas yang dipilih
+export async function getSiswaByKelasHariIni(kelasId: string) {
+  const res = await api.get(`/api/guru/absensi/siswa-hari-ini?kelasId=${kelasId}`);
+  return res.data;
+}
+
+// Kirim data absensi
+// services/guruService.ts
+export async function submitAbsensi(absensiData: AbsenSiswaItem[]) {
+  const res = await api.post("/api/guru/absensi", { absensiData }); // ✅ wrap dengan object
+  return res.data;
+}
+
+
+export async function getKelasYangDiajar() {
+  const res = await api.get("/api/guru/absensi/kelas-diajar");
+  return res.data;
+}
+
+export async function getRiwayatAbsensiByKelas(kelasId: string) {
+  const res = await api.get(`/api/guru/absensi/riwayat?kelasId=${kelasId}`);
+  return res.data;
+}
+
+
+// Ambil detail riwayat absensi berdasarkan kelasId
+export async function getDetailRiwayatAbsensi(kelasId: string) {
+  const res = await api.get(`/api/guru/absensi/riwayat/${kelasId}`);
   return res.data;
 }
