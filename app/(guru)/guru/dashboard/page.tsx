6a98e21fcaa77c2130be65dd3829af61e76a0d29
Joshtri <stuffofyos1516@gmail.com>
2025-08-05 23:37:37 +0700

    Commit: page.tsx

--- app/(guru)/guru/dashboard/page.tsx
+++ app/(guru)/guru/dashboard/page.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+// File: app/guru/dashboard/page.tsx
+
+"use client";
+
+import ChartAbsensiGuru from "@/components/Dashboard/DashboardGuru/ChartAbsensiGuru";
+import JadwalHariIni from "@/components/Dashboard/DashboardGuru/JadwalHariIni";
+import StatistikAbsensiHariIni from "@/components/Dashboard/DashboardGuru/StatistikAbsensiHariIni";
+import SummaryGuru from "@/components/Dashboard/DashboardGuru/SummaryGuru";
+
 
 export default function DashboardGuruPage() {
   return (
-    <div>DashboardPage</div>
-  )
+    <div className="space-y-6 p-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <SummaryGuru />
+        <StatistikAbsensiHariIni />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <JadwalHariIni />
+        <ChartAbsensiGuru/>
+        {/* <RekapAbsensiChart /> */}
+      </div>
+    </div>
+  );
 }
